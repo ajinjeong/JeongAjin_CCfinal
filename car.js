24d8a0e4819bff54535704a5922d6bba5ee1b9c6
ajinjeong <158008474+ajinjeong@users.noreply.github.com>
2024-12-16 02:37:19 -0500

    Final

--- car.js
+++ car.js
@@ -1,10 +1,10 @@
 //Milestone: I learned and experimented with 3D in p5, but decided it wasn't very suitable for this project, at least at my level. Below are what I used to learn and experiment with p5, but I mostly referenced the concepts, especially from the 3D Perspectives section in the WEBGL Fundamentals.
-  //The Coding Train: https://youtube.com/playlist?list=PLRqwX-V7Uu6bPhi8sS1hHJ77n3zRO9FR_&si=cyctCRPNajbyzsp5
-  //p5.js reference: https://p5js.org/reference/#3D
-  //WEBGL Fundamentals: https://webglfundamentals.org/webgl/lessons/webgl-3d-perspective.html
+//The Coding Train: https://youtube.com/playlist?list=PLRqwX-V7Uu6bPhi8sS1hHJ77n3zRO9FR_&si=cyctCRPNajbyzsp5
+//p5.js reference: https://p5js.org/reference/#3D
+//WEBGL Fundamentals: https://webglfundamentals.org/webgl/lessons/webgl-3d-perspective.html
 //notes to self:
-  //multiply steering by random acceleration value (--> lost sense of control)
-  //if you can't figure out how to manipulate the view, have the arrow keys do random things (ex: big truck starts driving head on, flashing lights/obstructed view)
+//multiply steering by random acceleration value (--> lost sense of control)
+//if you can't figure out how to manipulate the view, have the arrow keys do random things (ex: big truck starts driving head on, flashing lights/obstructed view)
 
 let lines = []; //array for lane lines
 let cars = []; //array for lane cars
@@ -14,18 +14,18 @@ function setup() {
 }
 
 function draw() {
-  background(130,200,229); //sky
+  background(130, 200, 229); //sky
   strokeWeight(5); //outline
   //grass
   fill(19, 118, 21);
-  rect(0, height/2,width, height/2);
+  rect(0, height / 2, width, height / 2);
   //road
   fill(150);
   quad(
     0, height,
     width, height,
-    width/2 + 100, height/2,
-    width/2 - 100, height/2
+    width / 2 + 100, height / 2,
+    width / 2 - 100, height / 2
   );
   //lane lines
   if (frameCount % 100 == 0) {
@@ -52,41 +52,41 @@ function draw() {
   //driver's view
   noFill();
   strokeWeight(100);
-  rect(0,0,width,height,100);
-  ellipse(350,height+100,600); //wheel
+  rect(0, 0, width, height, 100);
+  ellipse(350, height + 100, 600); //wheel
 }
 
 class Line { //lane line class
   constructor() {
     this.width = 10;
     this.height = 0;
-    this.position = createVector(width/2-5, height/2);
-    this.velocity = createVector(0,1);
+    this.position = createVector(width / 2 - 5, height / 2);
+    this.velocity = createVector(0, 1);
   }
   show() {
-    fill (255);
-    rect(this.position.x, this.position.y, this.width,this.height);
+    fill(255);
+    rect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
-      if (this.height < 50) {
-        this.height ++;
-      } else {
-        this.position.add(this.velocity);
-      }
+    if (this.height < 50) {
+      this.height++;
+    } else {
+      this.position.add(this.velocity);
+    }
   }
 }
 
 class Car { //car class
   constructor() {
     this.size = 50;
-    this.position = createVector(width/2, height/2-50);
-    this.velocity = createVector(0,1);
+    this.position = createVector(width / 2, height / 2 - 50);
+    this.velocity = createVector(0, 1);
   }
   show() {
     let scale = map(this.position.y, 0, height, 1, 10);
     let currentSize = this.size * scale;
-    fill (255);
-    rect(this.position.x-currentSize/2, this.position.y, currentSize, currentSize);
+    fill(255);
+    rect(this.position.x - currentSize / 2, this.position.y, currentSize, currentSize);
   }
   update() {
     this.position.add(this.velocity);
--- sketch.js
+++ sketch.js
@@ -1,99 +1,123 @@
-// //note to self:
-//   //animation (light flickering, movement)
-//   //speed up the lights appearing
-
-// function setup() {
-//   createCanvas(1470, 830, WEBGL);
-//   // debugMode();
-// }
-
-// function draw() {
-//   background(0);
-//   orbitControl();
-//   pointLight(255, 0, 0, 0, -300, 0);
-//   ambientLight(100);
-//   directionalLight(255, 255, 255, 1, -1, 0);
-
-//   //lightstick
-//   // push();
-//   // translate(0, 100, 0);
-//   // noStroke();
-//   // fill(200);
-//   // cylinder(30, 400);
-//   // pop();
-
-//   // for (let i = 0; i <= 200; i++) {
-//   //   push();
-//   //   let radius = map(i, 0, 200, 100, 50);
-//   //   translate(0, -200 + i, 0);
-//   //   rotateX(PI/2);
-//   //   noStroke();
-//   //   fill(200);
-//   //   torus(radius, 5);
-//   //   pop();
-//   // }
-
-//   // noStroke();
-//   // fill(200);
-//   // cylinder(50, 10);  
-// }
-
-let lights = [];
+let candle = false; //for controlling scenes
+let lights = []; //for creating lights
+let frame = 0; //to keep track of frames for animation
+let opacity = 255; //to control opacity of the building
+let candleColor; //to control color of the candle light
 
 function setup() {
-  createCanvas(1470, 830, WEBGL);
-  noStroke();
+  createCanvas(1470, 830, WEBGL); //fixed canvas size for more control over graphics, set rending mode to WEBGL for 3D
+  noStroke(); //remove stroke
+  loadPixels(); //load pixels for color picking for candle
 }
 
 function draw() {
-  background(0);
-  orbitControl();
-  // camera(0, -350, 500, 0, 0, 0, 0, 1, 0);
+  background(0); //set black background
+  orbitControl(); //allow orbit control
 
-  //National Assembly Building
-  push();
-  stroke(100);
-  fill(255);
-  translate(0, 0, -600);
-  box(300, 100, 100);
-  //dome
-  push();
-  translate(0, -50, 0);
-  fill(64, 224, 208);
-  sphere(50);
-  pop();
-  noStroke();
-  pop();
+  //scene #1
+  if (candle == false) {
+    camera(0, -350, 500, 0, 0, 0, 0, 1, 0); //set camera slightly above looking down
 
-  ambientLight(50);
+    //National Assembly Building
+    //building
+    push();
+    stroke(100, opacity); //gray stroke with opacity controlled
+    fill(255, opacity); //white fill with opacity controlled
+    translate(0, 0, -400); //move "back" into the screen (z direction)
+    box(600, 200, 300); //draw box
+    //dome
+    push();
+    translate(0, -80, 0); //move up (y direction) from where the box is drawn
+    fill(50, 204, 188, opacity); //dark minty fill with opacity controlled
+    sphere(130); //draw sphere
+    pop(); //pop dome push
+    pop(); //pop building push
 
-  if (lights.length < 1000 && random() < 0.05) {
-    lights.push(new Light());
+    ambientLight(50); //ambient lighting (after drawing building to not affect its color/fill)
+
+    //lights
+    if (lights.length < 1500 && random() < (0.2 * (1 + frame / 1000))) { //until there are 1500 lights. start by making the chance of creating a new light 3% but increase after each frame to speed up the creation of the lights
+      lights.push(new Light()); // push a new Light
+    }
+    for (let a = lights.length - 1; a >= 0; a--) { //interate through the lights array in reverse
+      lights[a].update(); //update the lights array
+      lights[a].show(); //show the lights array
+      frame++; //increase frame count (inside this for-loop to speed up the light creation after each created one is shown)
+    }
+
+    if (lights.length == 1500 && opacity >= 0) { //when all lights are drawn
+      opacity--; //decrease opacity of building until < zero (the building disappears)
+    }
   }
-  for (let a = lights.length - 1; a >= 0; a--) {
-    lights[a].update();
-    lights[a].show();
+
+  //scene #2
+  if (candle == true) {
+    let flickering = map(noise(frame * 0.05), 0, 1, 50, 200); //use noise to generate random values between 0 and the frame count (* 0.05 to scale because noise takes inputs between 0 and 1) and map it to a value between 50 and 200 (range for opacity) for flickering affect
+
+    pointLight(candleColor[0], candleColor[1], candleColor[2], 0, -300, 0); //the 'light' of the candle with colors controlled, moved up (y direction)
+    ambientLight(flickering); //set ambient light with rgb value determined by the flickering variable equation above
+    directionalLight(200, 200, 200, 1, -1, 0); //set gray directional light on the left to the right and up to show dimension of the candle
+
+    //candle
+    //stick
+    push();
+    translate(0, 100, 0); //move origin down (y direction)
+    fill(200);  //gray fill
+    cylinder(30, 400); //draw cylinder(r,h)
+    pop();
+    //cup
+    for (let i = 0; i <= 200; i++) { //interate through a loop about variable i starting from 0 to 200
+      push();
+      let radius = map(i, 0, 200, 100, 50); //map the i value (between 0 and 200) to a value between 100 and 50 and label it 'radius'
+      translate(0, -200 + i, 0); //move up 200 and down i
+      rotateX(PI / 2); //tilt the x axis 90 degrees to orient the torus horizontally
+      fill(200, 100);  //gray fill with small opacity
+      torus(radius, 5); //draw torus
+      pop();
+    }
+    //bottom of cup
+    fill(200, 255); //gray fill with full opacity
+    cylinder(50, 10); //draw disc(r,h)
+
+    frame++; //increase frame count (for flickering animation)
   }
+}
+
+function doubleClicked() { //when double clicked
+  candleColor = get(mouseX, mouseY); //get the pixel, store info in array [R, G, B, A] and name it 'candleColor'
+  candle = !candle; //toggle on/off candle scene (scene #2)
 }
 
 class Light {
   constructor() {
-    this.x = random(-1500 / 2, 1500 / 2);
-    this.z = random(-800 / 2, 800 / 2);
-    this.y = noise(this.x * 0.01, this.z * 0.01) * 100; // scale down inputs for closer => smoother. multiply by 100 to make y bigger (but still relatively flat). 
-    this.r = random(0, 255);
-    this.g = random(0, 255);
-    this.b = random(0, 255);
-    this.opacity = random(0, 255);
+    this.x = random(-2000 / 2, 2000 / 2); //generate random x coordinate for light position (in viewable range on screen)
+    this.z = random(-800 / 2, 800 / 2); //generate random z coordinate for light position (in viewable range on screen)
+    this.y = noise(this.x * 0.01, this.z * 0.01) * 100; //use noise to generate random values between/relative to x and z coordinates. scale down inputs for closer => smoother. multiply by 100 to make y bigger (but still relatively flat)
+    this.r = random(0, 255); //generate random r value
+    this.g = random(0, 255); //generate random g value
+    this.b = random(0, 255); //generate random b value
+    this.speed = noise(this.x * 0.01, this.z * 0.01) - 1; //use noise to generate random values between/relative to x and z coordinates.  
+    this.maxy = 100; //set max y to 100
+    this.miny = 0; //set min y to 0
   }
   show() {
     push();
-    translate(this.x, this.y, this.z);
-    fill(255);
-    emissiveMaterial(this.r, this.g, this.b, this.opacity);
-    sphere(3);
+    translate(this.x, this.y, this.z); //translate origin to xyz coordinate generated in constructor
+    fill(255); //white fill
+    emissiveMaterial(this.r, this.g, this.b); //set emissive color of surface material using rgb values generated in constructor
+    sphere(3); //draw sphere with radius 3
     pop();
   }
   update() {
+    if (lights.length == 1500) { //if there are 1500 lights
+      this.y += this.speed; //add speed to y position
+      if (opacity > 0 && this.y >= this.maxy || this.y <= this.miny) { //if the building opacity is decreasing but it is still visible && the y position of the light reaches min or max y
+        this.speed *= -1; //reverse direction of movement
+      } else if (opacity <= 0 && this.y >= this.maxy || this.y <= this.miny) { //if the building is not visible && the y position of the light reaches min or max y
+        this.speed *= -1; //reverse direction of movement
+        this.maxy += 100; //increase the max y by 100 to allow for greater movement after each loop
+        this.miny -= 100; //decrease the min y by 100 to allow for greater movement after each loop
+      }
+    }
   }
 }
